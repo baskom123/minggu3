849c2a97578c216a9ec6694641cfc7362cb03f81
baskom123 <116578096+baskom123@users.noreply.github.com>
2023-11-01 17:10:56 +0700

    Add files via upload

---
+++ HistoryList.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names
+
+class HistoryList {
+  int id;
+  String name;
+  int sum;
+  String tanggal;
+
+  HistoryList(this.id, this.name, this.sum, this.tanggal);
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'sum': sum, 'tanggal': tanggal};
+  }
+
+  @override
+  String toString() {
+    return 'id : $id\nname : $name\nsum : $sum\ntanggal : $tanggal';
+  }
+}
---
+++ Screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/m03/HistoryProvider.dart';
+import 'package:flutter_application_3/m03/HistoryScreen.dart';
+import 'package:flutter_application_3/m03/ItemScreen.dart';
+import 'package:flutter_application_3/m03/MyProvider.dart';
+import 'package:flutter_application_3/m03/ShoppingList.dart';
+import 'package:flutter_application_3/m03/dbhelper.dart';
+import 'package:flutter_application_3/m03/shopping_list_dialog.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+// import 'Provider/myProvider.dart';
+
+class Screen extends StatefulWidget {
+  const Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  int id = 0;
+  DBHelper _dbHelper = DBHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLastId();
+  }
+
+  Future<void> _loadLastId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      id = prefs.getInt('lastId') ?? 0;
+    });
+  }
+
+  Future<void> _saveLastId(int lastId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('lastId', lastId);
+  }
+
+  Future<void> _deleteAllShoppingList() async {
+    await _dbHelper.deleteAllShoppingList();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Shopping list deleted"),
+    ));
+    List<ShoppingList> shoppingList =
+        Provider.of<ListProductProvider>(context, listen: false)
+            .getShoppingList;
+    for (var item in shoppingList) {
+      await HistoryProvider.addDeletionToHistory(item.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var tmp = Provider.of<ListProductProvider>(context, listen: true);
+    _dbHelper.getmyShoppingList().then((value) => tmp.setShoppingList = value);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Shopping List"),
+        backgroundColor: Color.fromARGB(2, 182, 247, 5),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.delete),
+            tooltip: 'Delete All',
+            onPressed: () {
+              _deleteAllShoppingList();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.list),
+            tooltip: 'History',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HistoryScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount:
+              tmp.getShoppingList != null ? tmp.getShoppingList.length : 0,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(tmp.getShoppingList[index].id.toString()),
+                onDismissed: (direction) {
+                  String tmpName = tmp.getShoppingList[index].name;
+                  int tempId = tmp.getShoppingList[index].id;
+                  setState(() {
+                    tmp.deleteById(tmp.getShoppingList[index]);
+                  });
+                  _dbHelper.deleteShoppingList(tempId);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("$tmpName Delete"),
+                  ));
+                },
+                child: ListTile(
+                  title: Text(tmp.getShoppingList[index].name),
+                  leading: CircleAvatar(
+                    child: Text("${tmp.getShoppingList[index].sum}"),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return ItemScreen(tmp.getShoppingList[index]);
+                    }));
+                  },
+                  trailing: IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return ShoppingListDialog(_dbHelper).buildDialog(
+                                  context, tmp.getShoppingList[index], false);
+                            });
+                        _dbHelper
+                            .getmyShoppingList()
+                            .then((value) => tmp.setShoppingList = value);
+                      }),
+                ));
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog(
+              context: context,
+              builder: (context) {
+                return ShoppingListDialog(_dbHelper)
+                    .buildDialog(context, ShoppingList(++id, "", 0), true);
+              });
+          await _saveLastId(id); // Simpan ID terakhir
+          _dbHelper
+              .getmyShoppingList()
+              .then((value) => tmp.setShoppingList = value);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _dbHelper.closeDB();
+    super.dispose();
+  }
+}
